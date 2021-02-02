b48fcd84828e3b2944a4114e8fd331749cd45dd5
MauroCicerchia <maurocicerchia98@gmail.com>
2021-02-01 23:29:37 -0300

    feat: start project and add Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-indigo-50 dark:bg-gray-900 p-3">
+    <h1 class="text-gray-800 dark:text-indigo-50 text-2xl font-semibold">{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
